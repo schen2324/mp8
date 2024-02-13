c1ad41dd8fb2229ffc4dcf87167f9206fc61ccdd
schen2324 <schen@ifp-alguero.org>
2024-02-13 19:15:56 +0100

    cars

---
+++ webapp/js/script.js
@@ -0,0 +1,21 @@
+var fs = 1.5;
+
+document.getElementById("petit").addEventListener("click", petit);
+document.getElementById("mitja").addEventListener("click", mitja);
+document.getElementById("gran").addEventListener("click", gran);
+
+function petit() {
+    fs = fs - 0.5;
+    document.getElementsByClassName("text").style.fontSize=(fs+"rem");
+    // alert(fs+"rem");
+}
+function mitja() {
+    fs = 1.5;
+    document.getElementsByClassName("text").style.fontSize=(fs+"rem");
+    // alert(fs+"rem");
+}
+function gran() {
+    fs = fs + 0.5;
+    document.getElementsByClassName("text").style.fontSize=(fs+"rem");
+    // alert(fs+"rem");
+}
